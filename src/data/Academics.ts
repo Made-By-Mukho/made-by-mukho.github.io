674a14d17248f57df7331a78def54b92fd72e24c
Mukho <mukho@kakao.com>
2025-02-17 04:47:59 +0900

    Chore: 날짜 서식 변경

--- src/data/Academics.ts
+++ src/data/Academics.ts
@@ -5,7 +5,7 @@ export const academics: Academic[] = [
     id: 1,
     university: "경희대학교",
     major: "소프트웨어융합대학 컴퓨터공학부",
-    period: "2016.03 - 2023.02",
+    period: "2016.03. - 2023.02.",
     logoUrl:
       "https://yt3.googleusercontent.com/ytc/AIdro_kPkklM53rC-uk0KdHDp-KuSDtlVVRJkeeMC3U7FOy8ng=s160-c-k-c0x00ffffff-no-rj",
     description: "2022년도 경희대학교 봄 프로그래밍 경시대회 입상(장려상)",
--- src/data/Experiences.ts
+++ src/data/Experiences.ts
@@ -5,7 +5,7 @@ export const experiences: Experience[] = [
     id: 1,
     company: "(주)비즈테크아이",
     role: "Software Engineer",
-    period: "2023.09 - 2023.11",
+    period: "2023.09. - 2023.11.",
     description: [
       "[인턴] Java 개발자",
       "Spring(Java), React(JavaScript) 기반 웹 풀스택 개발",
