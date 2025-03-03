aea23c487f58e36feff0ba9664aae7fe58b60da4
Mukho <mukho@kakao.com>
2025-03-04 03:07:48 +0900

    Chore: 아 맞다 우산 복구

--- src/data/Projects.ts
+++ src/data/Projects.ts
@@ -4,7 +4,7 @@ import scrank from "../assets/images/scrank.png";
 import underworld from "../assets/images/underworld.png";
 import mlock from "../assets/images/mlock.png";
 import ssammu from "../assets/images/ssammu.png";
-// import umbrella from "../assets/images/umbrella.png";
+import umbrella from "../assets/images/umbrella.png";
 import questionbank from "../assets/images/questionbank.png";
 // import edubox from "../assets/images/edubox.png";
 import mbm from "../assets/images/mbm.png";
@@ -52,14 +52,14 @@ export const projects: Project[] = [
     projectUrl: "https://ssammutalk.mukho.r-e.kr/",
     githubUrl: "https://github.com/mukhoplus/SsamMuTalk_docs",
   },
-  // {
-  //   id: 6,
-  //   title: "아 맞다 우산",
-  //   description: "우산 알림 애플리케이션",
-  //   imageUrl: umbrella,
-  //   projectUrl: "https://github.com/Mukho-I-O",
-  //   githubUrl: "https://github.com/Mukho-I-O",
-  // },
+  {
+    id: 6,
+    title: "아 맞다 우산",
+    description: "우산 알림 애플리케이션",
+    imageUrl: umbrella,
+    projectUrl: "https://github.com/Mukho-I-O",
+    githubUrl: "https://github.com/Mukho-I-O/Oh-My-Umbrella-Prototype",
+  },
   {
     id: 7,
     title: "모의고사 생성기",
