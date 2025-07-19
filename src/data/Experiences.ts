2b7720e0c7aa7528c2a8aea475cdcdeaa3e9a880
Mukho <mukho@kakao.com>
2025-07-20 00:07:45 +0900

    Chore: 인턴 내용 순서 변경

--- src/data/Experiences.ts
+++ src/data/Experiences.ts
@@ -9,8 +9,8 @@ export const experiences: Experience[] = [
     description: [
       "[인턴] Java 개발",
       "Spring(Java), React(JavaScript) 기반 웹 풀스택 개발",
-      "LG CNS AM Incubation Camp 수료",
       "개인 프로젝트(FE/TA), 팀 프로젝트 각 2회 수행",
+      "LG CNS AM Incubation Camp 수료",
     ],
   },
 ];
